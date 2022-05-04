065ac9aae1b2e61ab77a3038196caf722f16c193
alexa <alexandreordas@yahoo.com>
2022-05-05 01:53:22 +0200

    Update index.js change render method

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom";
 import App from "./components/App"
 import "bootstrap/dist/css/bootstrap.css"
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App tab="home" />);
+
